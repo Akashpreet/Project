33b85b63a1a3a944825ef54699c42bf4248e66e0
Akashpreet <akash1234560987@gmail.com>
2023-12-11 20:36:08 +0530

    done for 11 dec

--- router/auth.js
+++ router/auth.js
@@ -1,8 +1,9 @@
 const express=require('express');
 // const { router } = require('./router');
 const {login,logout,signup}=require('../functions/auth')
+const {checkCreds}=require("../middleware/checkCreds");
 const authrouter=express.Router()
 authrouter.get('/login',login)
 authrouter.get('/logout',logout)
-authrouter.post('/signup',signup)
+authrouter.post('/signup',checkCreds,signup)
 module.exports={authrouter}
