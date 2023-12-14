e35f354152e5f17f9478a2c055b1f1d5ead39306
Akashpreet <101885362+Akashpreet@users.noreply.github.com>
2023-12-14 20:08:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 // mongoose.connect('mongoose://127.0.0.1:27017/Class Project')
 
 const connectToMongo = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/ClassProject').then(() => { console.log("Successfully Connected") }).catch(() => { console.log("Conection Failed") })
+    mongoose.connect('mongodb+srv://akash123:qd8JaAK1eIVSzT48@cluster0.4jh1kdn.mongodb.net/?retryWrites=true&w=majority').then(() => { console.log("Successfully Connected") }).catch(() => { console.log("Conection Failed") })
 }
 module.exports = { connectToMongo }
